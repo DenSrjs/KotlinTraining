4cf46e978e90215d8d20a57cca873108fd5197ea
denal <denalexsr@ya.ru>
2023-03-31 13:58:00 +0300

    KT-5-3 fixed

--- src/main/kotlin/lesson5/lesson5-task3.kt
+++ src/main/kotlin/lesson5/lesson5-task3.kt
@@ -1,23 +1,25 @@
 package lesson5
 
 fun main() {
+    val numberOne = 56
+    val numberTwo = 87
+
     println("Введите два целых числа от 1 до 100")
 
     print("Введите первое число: ")
-    val inputNumberOne: Byte = readlnOrNull().toString().toByte()
+    val inputNumberOne = readln().toInt()
 
     print("Введите второе число: ")
-    val inputNumberTwo: Byte = readlnOrNull().toString().toByte()
-
-    val numberOne: Byte = 56
-    val numberTwo: Byte = 87
+    val inputNumberTwo = readln().toInt()
 
     println("Ваши числа $inputNumberOne, $inputNumberTwo")
 
     if ((inputNumberOne == numberOne || inputNumberOne == numberTwo)
-        && (inputNumberTwo == numberOne || inputNumberTwo == numberTwo)) println( "Поздравляем! Вы выиграли главный приз!")
-    else if((inputNumberOne == numberOne || inputNumberOne == numberTwo)
-        || (inputNumberTwo == numberOne || inputNumberTwo == numberTwo)) println("Вы выиграли утешительный приз")
+        && (inputNumberTwo == numberOne || inputNumberTwo == numberTwo)
+    ) println("Поздравляем! Вы выиграли главный приз!")
+    else if ((inputNumberOne == numberOne || inputNumberOne == numberTwo)
+        || (inputNumberTwo == numberOne || inputNumberTwo == numberTwo)
+    ) println("Вы выиграли утешительный приз")
     else println("Неудача! Крутите барабан!")
 
     println("Победные числа $numberOne и $numberTwo")
