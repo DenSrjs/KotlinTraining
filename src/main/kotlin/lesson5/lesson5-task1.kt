55a1951517f4e57cf97f94a1df26dc8a780610db
denal <denalexsr@ya.ru>
2023-03-31 13:49:41 +0300

    KT-5-1

---
+++ src/main/kotlin/lesson5/lesson5-task1.kt
@@ -0,0 +1,10 @@
+package lesson5
+
+fun main() {
+    print("Для входа в систему решите пример 2 + 2 = ")
+
+    val inputNumber = readln().toInt()
+
+    if (inputNumber == 4) println("Добро пожаловать")
+    else println("Доступ запрещен")
+}
